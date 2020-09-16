14299e3ca6e304c7a1f61c25d7dd7e01a76b7be9
Pablo Coronel <me@pablitxn.io>
2020-09-16 02:00:53 -0300

    feat(var-env): se configuran variables de entorno correctamente

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,12 +1,12 @@
 // App
-export const API_KEY_HEROKU = process.env.API_KEY_HEROKU;
-export const GOOGLE_SITE_VERIFICATION = process.env.GOOGLE_SITE_VERIFICATION;
+export const API_KEY_HEROKU = process.env.NEXT_PUBLIC_API_KEY_HEROKU;
+export const GOOGLE_SITE_VERIFICATION = process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION;
 export const DEFAULT_LANGUAGE = "es-ES"
 
 // Covid Map
-export const API_COVID_ALL = process.env.API_COVID_ALL;
-export const API_COVID_BRIEF = process.env.API_COVID_BRIEF;
-export const API_KEY_GOOGLE_MAPS = process.env.API_KEY_GOOGLE_MAPS;
+export const API_COVID_ALL = process.env.NEXT_PUBLIC_API_COVID_ALL;
+export const API_COVID_BRIEF = process.env.NEXT_PUBLIC_API_COVID_BRIEF;
+export const API_KEY_GOOGLE_MAPS = process.env.NEXT_PUBLIC_API_KEY_GOOGLE_MAPS;
 
 // Ecommerce
-export const API_PLAYGROUND_BACKEND = process.env.API_PLAYGROUND_BACKEND;
+export const API_PLAYGROUND_BACKEND = process.env.NEXT_PUBLIC_API_PLAYGROUND_BACKEND;
