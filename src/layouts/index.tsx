b932c40e6a93f3469700b010472ed1d6222c9540
Pablo Coronel <me@pablitxn.io>
2020-09-21 14:00:16 -0300

    feat(layout): se quita el menu en pantallas pequeñas

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,10 +1,21 @@
-// Types
+// React
 import { FC, ReactNode, useEffect, useState } from "react";
+// Components
+import Navigation from "components/_shared/nativagation";
 // AntD
 import { Layout, Menu } from "antd";
-import Navigation from "components/_shared/nativagation";
+import {
+	HomeOutlined,
+	GlobalOutlined,
+	VideoCameraOutlined,
+	ShopOutlined,
+	AuditOutlined,
+	PlayCircleOutlined
+} from "@ant-design/icons";
 // Router
 import { useRouter } from "next/router";
+// Styles
+import "./styles.less";
 
 // Definitions
 const { Content, Sider } = Layout;
@@ -25,32 +36,50 @@ const AppLayout: FC<IAppLayout> = ({ children }) => {
 
 	const { collapsed, defaultKey } = state;
 
+	/**
+	 * <HomeOutlined, />
+	 * <GlobalOutlined, />
+	 * <VideoCameraOutlined, />
+	 * <ShopOutlined, />
+	 * <AuditOutlined, />
+	 * <PlayCircleOutlined, />
+	 *
+	 */
+
 	return (
-		<Layout style={{ minHeight: "100vh" }}>
-			<Sider collapsible collapsed={collapsed} onCollapse={handleCollapse}>
+		<Layout className="app-layout">
+			<Sider
+				collapsible
+				collapsed={collapsed}
+				onCollapse={handleCollapse}
+				className="app-layout__slider"
+			>
 				<div className="logo" />
 				<Menu theme="dark" mode="inline" defaultSelectedKeys={defaultKey}>
-					<Menu.Item key="/">
+					<Menu.Item icon={<HomeOutlined />} key="/">
 						<Navigation href="/">Welcome</Navigation>
 					</Menu.Item>
-					<Menu.Item key="/covid-map">
+					<Menu.Item icon={<GlobalOutlined />} key="/covid-map">
 						<Navigation href="/covid-map">Covid-Map</Navigation>
 					</Menu.Item>
-					<Menu.Item key="/chroma-key">
+					<Menu.Item icon={<VideoCameraOutlined />} key="/chroma-key">
 						<Navigation href="/chroma-key">Croma customizable</Navigation>
 					</Menu.Item>
-					<Menu.Item key="/ecommerce">
+					<Menu.Item icon={<ShopOutlined />} key="/ecommerce">
 						<Navigation href="/ecommerce">Ecommerce</Navigation>
 					</Menu.Item>
-					<Menu.Item key="/backoffice">
+					<Menu.Item icon={<AuditOutlined />} key="/backoffice">
 						<Navigation href="/backoffice">Backoffice</Navigation>
 					</Menu.Item>
-					<SubMenu key="submenu" title="Random">
+					<SubMenu icon={<PlayCircleOutlined />} key="submenu" title="Random">
 						<Menu.Item key="/ta-te-ti">Ta-te-ti</Menu.Item>
 						<Menu.Item key="/slider-360">Slider 360</Menu.Item>
 					</SubMenu>
 				</Menu>
 			</Sider>
+			<Navigation href="/" className="app-layout__home-button">
+				<HomeOutlined />
+			</Navigation>
 			<Layout className="site-layout">
 				<Content style={{ margin: "0 16px" }}>{children}</Content>
 			</Layout>
